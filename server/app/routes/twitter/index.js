65e5e31e4759a8b585e653803289470c05ddcdb5
calvinnhobbes <cschoi3@gmail.com>
2015-06-18 17:41:57 -0400

    Got bot to reply when sent a link

--- server/app/routes/twitter/index.js
+++ server/app/routes/twitter/index.js
@@ -1,6 +1,8 @@
 var Twitter = require('twitter');
 var es = require('event-stream');
 var router = require('express').Router();
+var q = require('q');
+
 
 module.exports = router;
 
@@ -18,14 +20,22 @@ client.get('statuses/user_timeline', params, function(error, tweets, response){
         console.log("this works!", tweets[1].text);
     console.log("this works!", tweets[2].text);
 
-
   }
 });
 
 client.stream('statuses/filter', {track: "DaDeetzPlz"}, function(stream) {
   console.log('We are listening E.T.:')
   stream.on('data', function(tweet) {
-    console.log("inside stream:",tweet.entities.urls[0]);
+    // functionality for multi links
+    // var x = 1;
+    //   if(tweet.entities.urls.length > 1){
+    //     for(var i = 0; i < tweet.entities.urls.length; i++){
+    //       reply(tweet.user.screen_name, tweet.entities.urls[i].expanded_url, x++, tweet.entities.urls.length );
+    //     }
+    //   } else {
+    // }
+        reply(tweet.user.screen_name, tweet.entities.urls[0].expanded_url);
+      
   });
 
   stream.on('error', function(error) {
@@ -33,8 +43,18 @@ client.stream('statuses/filter', {track: "DaDeetzPlz"}, function(stream) {
   });
 });
 
-
-// client.post('statuses/update', { status: 'hello world! pt2' }, function(err, data, response) {
+// client.post('statuses/update', { status: '@jmeshen hello world!' }, function(err, data, response) {
 //   console.log(data)
 // })
+
+
+var reply = function(user, link, index, length) {
+  client.post('statuses/update', { status: '@' + user + "this yo link: " + link}, function(err, data, response) {
+        console.log(data)
+      })
+  // post for multi links
+  // client.post('statuses/update', { status: '@' + user +" " + index +"of" +length + "this yo link:" + link}, function(err, data, response) {
+  //       console.log(data)
+  //     })
+}
 
